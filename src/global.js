f8097e5687c33b976de5521d17fc1aada2dc2087
hugoferreirag <hugo.fersoft@gmail.com>
2019-10-28 23:36:03 -0300

    as

--- src/global.js
+++ src/global.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 
 export const userKey = '__knowledge_user'
-export const baseApiUrl = 'https://fronte.herokuapp.com'
+export const baseApiUrl = 'https://backendtccfer.herokuapp.com'
 
 export function showError(e) {
     if(e && e.response && e.response.data) {
