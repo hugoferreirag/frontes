adf3248e2fa925455eab7fe1b6c82514f2ab8138
hugoferreirag <50091163+hugoferreirag@users.noreply.github.com>
2019-11-04 15:48:50 -0300

    Update Auth.vue

--- src/components/auth/Auth.vue
+++ src/components/auth/Auth.vue
@@ -91,7 +91,7 @@ export default {
         align-items: center;
     }
     .auth-modal {
-        background-color: #FFF;
+        background-color:red;
         width: 350px;
         padding: 35px;
         box-shadow: 0 1px 5px rgba(0, 0, 0, 0.15);
