8ac3a3822c3f28b70b52f3b38cd11feb2163b112
hugoferreirag <50091163+hugoferreirag@users.noreply.github.com>
2019-11-04 12:28:45 -0300

    Update Auth.vue

--- src/components/auth/Auth.vue
+++ src/components/auth/Auth.vue
@@ -4,15 +4,16 @@
             <img src="@/assets/Sem título.png" width="200" alt="Logo" />
             <hr>
             <div class="auth-title">{{ showSignup ? 'Cadastro' : 'Login' }}</div>
+             <label v-if="showSignup" for="cientista">É um cientista da Computação ?</label>   
+            <input v-model="user.cientista" v-if="showSignup" name="cientista" type="checkbox" placeholder=""> 
+            <input disabled v-if="user.cientista === true && showSignup " value="Sim :)" type="text">
+            <input disabled v-else-if="user.cientista === false && showSignup" value="Não :(" type="text">
 
             <input v-if="showSignup" append-icon="place" v-model="user.name" type="text" placeholder="Nome">
             <input v-model="user.email" name="email" type="text" placeholder="E-mail">
             <label v-if="showSignup" for="">Data de nascimento</label>
             <input v-if="showSignup" v-model="user.nascimento" name="data" type="date" placeholder="Data de Nascimento">
-            <label v-if="showSignup" for="cientista">É um cientista da Computação ?</label>   
-            <input v-model="user.cientista" v-if="showSignup" name="cientista" type="checkbox" placeholder=""> 
-            <input disabled v-if="user.cientista === true && showSignup " value="Sim :)" type="text">
-            <input disabled v-else-if="user.cientista === false && showSignup" value="Não :(" type="text">
+           
             <div v-else></div>
             <input v-model="user.password" name="password" type="password" placeholder="Senha">
             <input v-if="showSignup" v-model="user.confirmPassword"
