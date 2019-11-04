4b1189dc2c0b0bfd01077583b3bf20fb2df9a224
hugoferreirag <50091163+hugoferreirag@users.noreply.github.com>
2019-11-04 15:55:57 -0300

    Update Auth.vue

--- src/components/auth/Auth.vue
+++ src/components/auth/Auth.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="auth-content">
         <div class="auth-modal">
-            <img src="@/assets/logo.png" width="200" alt="Logo" />
+            <img v-if="showSignup" src="@/assets/logo.png" width="200" alt="Logo" />
+             <img v-if="!showSignup" src="@/assets/Sem título.png" width="200" alt="Logo" />
             <hr>
             <div class="h4 text-center mb-4">{{ showSignup ? 'Cadastro' : 'Login' }}</div>
              <label v-if="showSignup" for="cientista">É um cientista da Computação ?</label> 
