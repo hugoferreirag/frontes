564ef60c538d842ffc4e113a8aa3268d8521cf1e
Fernanda Araujo Pedace <fernandaped@hotmail.com>
2019-11-05 11:56:24 -0300

    arrumando auth

--- src/components/template/Content.vue
+++ src/components/template/Content.vue
@@ -13,11 +13,7 @@ export default {
 <style>
     .content {
         grid-area: content;
-<<<<<<< HEAD
         background-color:red !important;
-=======
-        background-image: url("~@/assets/giphy.gif");
->>>>>>> 238e6cb4e9b44e3668e8315675acec5b2bd4a0b8
         padding: 20px;
     }
 </style>
